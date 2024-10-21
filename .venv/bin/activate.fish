c16af5a2199b02ce6f65f8cb34be3d3c6b9e7e37
Your Name <you@example.com>
2024-10-21 17:39:52 -0400

    Re-implementing 'render.yaml', changes in Directories: '.venv/bin/activate' / '.venv/bin/activate.csh' / '.venv/bin/activate.fish' / '.venv/pyenv.cfg'

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/data/data/com.termux/files/home/projects/Scheduling-Tool-GitRb/.venv"
+set -gx VIRTUAL_ENV "/data/data/com.termux/files/home/projects/Scheduling-Tool/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
